e9f762c06d825055dadc677311405d04961a070a
Marko <zedbyte1@outlook.com>
2025-02-09 16:50:39 +0200

    Products CRUD

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid Id, string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+public record UpdateProductResponse(bool IsSuccess);
+public class UpdateProductEndpoint() : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(request.Adapt<UpdateProductCommand>());
+            var response = result.Adapt<UpdateProductResponse>();
+
+            return Results.Ok(response);
+
+        })
+        .WithName("UpdateProduct")
+        .Produces<UpdateProductEndpoint>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
